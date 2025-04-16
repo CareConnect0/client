aac5fb729ade6685824a618b6acb5bb44c96de2e
kimjihyun <kimjihyun@MacBook-Pro.local>
2025-04-16 18:54:06 +0900

    feat: main screen (#6)

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,91 +1,183 @@
 import 'package:client/designs/CareConnectColor.dart';
 import 'package:client/designs/CareConnectTypo.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class Home extends ConsumerWidget {
+  Home({super.key});
+
+  final calendarNotificationProvider = StateProvider<bool>((ref) => true);
+  final messengerNotificationProvider = StateProvider<bool>((ref) => true);
+  final AINotificationProvider = StateProvider<bool>((ref) => true);
+  final emergencyNotificationProvider = StateProvider<bool>((ref) => true);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final hasCalendarNotification = ref.watch(calendarNotificationProvider);
+    final hasMessengerNotification = ref.watch(messengerNotificationProvider);
+    final hasAINotification = ref.watch(AINotificationProvider);
+    final hasEmergencyNotification = ref.watch(emergencyNotificationProvider);
+
     return Scaffold(
-        body: Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-            colorFilter: ColorFilter.mode(
-                CareConnectColor.black.withOpacity(0.65), BlendMode.srcOver),
-            image: AssetImage("assets/images/background.png"),
-            fit: BoxFit.cover),
-      ),
-      child: Center(
+      appBar: AppbarWidget(),
+      body: Padding(
+        padding: const EdgeInsets.all(38.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SvgPicture.asset("assets/images/logo.svg"),
-            SizedBox(
-              height: 15,
-            ),
-            Semibold_28px(
-              text: "함께하루",
-              color: CareConnectColor.primary[900],
-            ),
-            SizedBox(
-              height: 75,
-            ),
-            InkWell(
-              onTap: () {
-                context.go('/signIn');
-              },
-              child: Container(
-                margin: EdgeInsets.symmetric(horizontal: 51),
-                padding: EdgeInsets.symmetric(vertical: 16),
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: CareConnectColor.primary[900],
-                ),
-                child: Center(
-                  child: Bold_22px(
-                    text: "로그인",
-                    color: CareConnectColor.neutral[50],
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: MenuCard(context, '/', CareConnectColor.primary[900],
+                        "달력", "assets/icons/calendar.svg",
+                        hasNotification: hasCalendarNotification),
                   ),
-                ),
+                  SizedBox(
+                    width: 13,
+                  ),
+                  Expanded(
+                    child: MenuCard(context, '/', CareConnectColor.primary[400],
+                        "메신저", "assets/icons/mail.svg",
+                        hasNotification: hasMessengerNotification),
+                  ),
+                ],
               ),
             ),
             SizedBox(
-              height: 22,
+              height: 13,
             ),
-            Medium_16px(
-              text: "아직 회원이 아니신가요?",
-              color: CareConnectColor.neutral[50],
-            ),
-            SizedBox(
-              height: 11,
-            ),
-            InkWell(
-              onTap: () {
-                context.go('/signUp');
-              },
-              child: Container(
-                margin: EdgeInsets.symmetric(horizontal: 51),
-                padding: EdgeInsets.symmetric(vertical: 16),
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: CareConnectColor.primary[200],
-                ),
-                child: Center(
-                  child: Bold_22px(
-                    text: "회원가입",
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: MenuCard(context, '/', CareConnectColor.primary[200],
+                        "AI\n대화 도우미", "assets/icons/user.svg",
+                        hasNotification: hasAINotification),
                   ),
-                ),
+                  SizedBox(
+                    width: 13,
+                  ),
+                  Expanded(
+                    child: MenuCard(
+                        context,
+                        '/',
+                        CareConnectColor.secondary[500],
+                        "비상 호출",
+                        "assets/icons/emergency-call.svg",
+                        hasNotification: hasEmergencyNotification),
+                  ),
+                ],
               ),
             ),
           ],
         ),
       ),
-    ));
+    );
+  }
+
+  PreferredSizeWidget AppbarWidget() {
+    return PreferredSize(
+      preferredSize: Size.fromHeight(160),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 28, vertical: 20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(16),
+              bottomRight: Radius.circular(16)),
+          color: CareConnectColor.primary[900],
+          boxShadow: [
+            BoxShadow(
+              color: const Color(0xFFFF9800),
+              blurRadius: 5,
+              offset: Offset(0, 0),
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Row(
+              children: [
+                Spacer(),
+                SvgPicture.asset('assets/icons/bell.svg'),
+                SizedBox(width: 12),
+                SvgPicture.asset('assets/icons/settings.svg'),
+              ],
+            ),
+            Bold_26px(
+              text: "안녕하세요, 사용자님",
+              color: CareConnectColor.white,
+            ),
+            Medium_20px(
+              text: "오늘도 좋은 하루 되세요.",
+              color: CareConnectColor.white,
+            ),
+            SizedBox(
+              height: 20,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget MenuCard(
+      BuildContext context, link, color, String text, String assetName,
+      {bool hasNotification = false}) {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        InkWell(
+          onTap: () {
+            context.go(link);
+          },
+          child: Container(
+            width: double.infinity,
+            padding: EdgeInsets.symmetric(vertical: 27),
+            decoration: BoxDecoration(
+              color: color,
+              borderRadius: BorderRadius.circular(16),
+              boxShadow: [
+                BoxShadow(
+                  color: const Color(0xFF939393).withOpacity(0.25),
+                  blurRadius: 8,
+                  offset: Offset(0, 0),
+                ),
+              ],
+            ),
+            child: Column(
+              children: [
+                Semibold_22px(
+                  text: text,
+                  textAlign: TextAlign.center,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 30),
+                    child: SvgPicture.asset(assetName),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        if (hasNotification)
+          Positioned(
+            top: -3,
+            right: -3,
+            child: Container(
+              width: 20,
+              height: 20,
+              decoration: BoxDecoration(
+                color: CareConnectColor.secondary[500],
+                shape: BoxShape.circle,
+              ),
+            ),
+          ),
+      ],
+    );
   }
 }
---
+++ lib/screens/splashSign.dart
@@ -0,0 +1,91 @@
+import 'package:client/designs/CareConnectColor.dart';
+import 'package:client/designs/CareConnectTypo.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
+
+class SplashSign extends StatelessWidget {
+  const SplashSign({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            colorFilter: ColorFilter.mode(
+                CareConnectColor.black.withOpacity(0.65), BlendMode.srcOver),
+            image: AssetImage("assets/images/background.png"),
+            fit: BoxFit.cover),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SvgPicture.asset("assets/images/logo.svg"),
+            SizedBox(
+              height: 15,
+            ),
+            Semibold_28px(
+              text: "함께하루",
+              color: CareConnectColor.primary[900],
+            ),
+            SizedBox(
+              height: 75,
+            ),
+            InkWell(
+              onTap: () {
+                context.go('/signIn');
+              },
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 51),
+                padding: EdgeInsets.symmetric(vertical: 16),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: CareConnectColor.primary[900],
+                ),
+                child: Center(
+                  child: Bold_22px(
+                    text: "로그인",
+                    color: CareConnectColor.neutral[50],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 22,
+            ),
+            Medium_16px(
+              text: "아직 회원이 아니신가요?",
+              color: CareConnectColor.neutral[50],
+            ),
+            SizedBox(
+              height: 11,
+            ),
+            InkWell(
+              onTap: () {
+                context.go('/signUp');
+              },
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 51),
+                padding: EdgeInsets.symmetric(vertical: 16),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: CareConnectColor.primary[200],
+                ),
+                child: Center(
+                  child: Bold_22px(
+                    text: "회원가입",
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
