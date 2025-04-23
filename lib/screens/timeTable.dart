0075d5f303d7186da1bd976bdb85054f74a45218
kimjihyun <kimjihyun@MacBook-Pro.local>
2025-04-23 23:57:59 +0900

    feat: schedule modify dialog (#9)

--- lib/screens/timeTable.dart
+++ lib/screens/timeTable.dart
@@ -1,28 +1,59 @@
 import 'package:client/designs/CareConnectButton.dart';
 import 'package:client/designs/CareConnectColor.dart';
+import 'package:client/designs/CareConnectDialog.dart';
 import 'package:client/designs/CareConnectTypo.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
+
+final scheduleProvider =
+    StateNotifierProvider<ScheduleNotifier, Map<String, List<String>>>(
+  (ref) => ScheduleNotifier(),
+);
+
+class ScheduleNotifier extends StateNotifier<Map<String, List<String>>> {
+  ScheduleNotifier()
+      : super({
+          '07:00': ['비타민 복용', '아침 식사'],
+          '10:00': ['회의'],
+          '14:00': ['병원 예약', '내시경 검사 받으러 가기 ( 병원명 )'],
+          '15:00': ['병원 예약', '내시경 검사 받으러 가기 ( 병원명 )'],
+          '16:00': ['병원 예약', '내시경 검사 받으러 가기 ( 병원명 )'],
+        });
+
+  void modifySchedule(String time, String oldEvent, String newEvent) {
+    final updated = [...state[time]!];
+    final index = updated.indexOf(oldEvent);
+    if (index != -1) {
+      updated[index] = newEvent;
+      state = {
+        ...state,
+        time: updated,
+      };
+    }
+  }
+
+  void deleteSchedule(String time, String event) {
+    final updated = [...state[time]!];
+    updated.remove(event);
+    state = {
+      ...state,
+      time: updated,
+    };
+  }
+}
 
 class TimeTable extends ConsumerWidget {
   final DateTime selected;
 
   TimeTable(this.selected, {super.key});
 
-  // 예시용 일정 목록
-  final Map<String, List<String>> schedule = {
-    '07:00': ['비타민 복용', '아침 식사'],
-    '10:00': ['회의'],
-    '14:00': ['병원 예약', '내시경 검사 받으러 가기 ( 병원명 )'],
-    // '15:00': ['병원 예약', '내시경 검사 받으러 가기 ( 병원명 )'],
-    // '16:00': ['병원 예약', '내시경 검사 받으러 가기 ( 병원명 )'],
-    // '17:00': ['병원 예약', '내시경 검사 받으러 가기 ( 병원명 )'],
-  };
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final schedule = ref.watch(scheduleProvider);
+
     return FractionallySizedBox(
       heightFactor: (MediaQuery.of(context).size.height - 100) /
           MediaQuery.of(context).size.height,
@@ -89,22 +120,64 @@ class TimeTable extends ConsumerWidget {
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: eventsAtTime.map((event) {
+                                final controller =
+                                    TextEditingController(text: event);
+
                                 return Padding(
                                   padding: const EdgeInsets.only(bottom: 8),
-                                  child: Row(
-                                    children: [
-                                      Container(
-                                        width: 22,
-                                        height: 22,
-                                        decoration: const BoxDecoration(
-                                          shape: BoxShape.circle,
+                                  child: InkWell(
+                                    onTap: () {
+                                      showDialog(
+                                        context: context,
+                                        builder: (BuildContext context) {
+                                          return CareConnectDialog(
+                                            time: time,
+                                            scheduleText: event,
+                                            scheduleController: controller,
+                                            cancel: () {
+                                              controller.text = event;
+                                              context.pop();
+                                            },
+                                            modify: () {
+                                              ref
+                                                  .read(
+                                                      scheduleProvider.notifier)
+                                                  .modifySchedule(
+                                                    time,
+                                                    event,
+                                                    controller.text,
+                                                  );
+                                              context.pop();
+                                            },
+                                            delete: () {
+                                              ref
+                                                  .read(
+                                                      scheduleProvider.notifier)
+                                                  .deleteSchedule(time, event);
+                                              context.pop();
+                                            },
+                                          );
+                                        },
+                                      );
+                                    },
+                                    child: Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Container(
+                                          width: 22,
+                                          height: 22,
+                                          decoration: const BoxDecoration(
+                                            shape: BoxShape.circle,
+                                          ),
+                                          child: SvgPicture.asset(
+                                              'assets/icons/person.svg'),
                                         ),
-                                        child: SvgPicture.asset(
-                                            'assets/icons/person.svg'),
-                                      ),
-                                      const SizedBox(width: 8),
-                                      Expanded(child: Medium_16px(text: event)),
-                                    ],
+                                        const SizedBox(width: 8),
+                                        Expanded(
+                                            child: Medium_16px(text: event)),
+                                      ],
+                                    ),
                                   ),
                                 );
                               }).toList(),
