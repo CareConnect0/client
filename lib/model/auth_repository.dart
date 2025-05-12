7b2e36fc1417074c9430bfa4d877f3d15d4f4b47
kimjihyun <kimjihyun@MacBook-Pro.local>
2025-05-12 13:47:17 +0900

    feat: logout api (#44)

--- lib/model/auth_repository.dart
+++ lib/model/auth_repository.dart
@@ -1,4 +1,5 @@
 import 'package:client/model/auth_storage.dart';
+import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
 
 class AuthRepository {
@@ -27,6 +28,26 @@ class AuthRepository {
       );
     } else {
       throw Exception('토큰 재발급 실패: ${response.reasonPhrase}');
+    }
+  }
+
+  final _dio = Dio(BaseOptions(baseUrl: 'http://3.38.183.170:8080'));
+
+  Future<void> logout() async {
+    final accessToken = await AuthStorage.getAccessToken();
+    print('🔑 로그아웃 함수 진입 - accessToken: $accessToken');
+
+    try {
+      await _dio.patch(
+        '/api/auth/logout',
+        options: Options(
+          headers: {'Authorization': accessToken},
+        ),
+      );
+    } catch (e) {
+      print('❌ 로그아웃 API 호출 실패: $e');
+    } finally {
+      await AuthStorage.clear();
     }
   }
 }
