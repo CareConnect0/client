120fdb6857fc0b00651f71cd0c5eff7d8190e38b
kimjihyun <kimjihyun@MacBook-Pro.local>
2025-05-21 17:01:22 +0900

    fix: flutter_sound library의 wav파일 error fix (#91)

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,12 @@ import Foundation
 
 import flutter_secure_storage_macos
 import path_provider_foundation
+import taudio
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  TaudioPlugin.register(with: registry.registrar(forPlugin: "TaudioPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
